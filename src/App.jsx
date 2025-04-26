8f9fa7b4762c284af3d117c4a29b857b9bbb3bea
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-04-26 13:40:59 +0530

    Added react routes package and added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import Favourites from './pages/Favourites';
+import NavBar from './components/Navbar';
 
 function App() {
   const movieNumber = 1;
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/favourites" element={<Favourites></Favourites>}></Route>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
